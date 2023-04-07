810c955bef6d9cde2718159283102264c076e1da
nanaopoku2 <nanaopoku396@gmail.com>
2023-04-07 02:09:01 -0700

    logging and exception

---
+++ src/logger.py
@@ -0,0 +1,22 @@
+import logging
+import os
+from datetime import datetime
+
+
+LOG_FIlE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FIlE)
+os.makedirs(logs_path,exist_ok=True)
+
+
+LOG_FIlE_PATH=os.path.join(logs_path,LOG_FIlE)
+
+
+logging.basicConfig(
+    filename=LOG_FIlE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# # Tesing the logger
+# if __name__=="__main__":
+#     logging.info("Logging has started")
